07a9e1263b7654413e9d096a6a460a1ef706d263
Bishal Shrestha <b.shresth12@gmail.com>
2024-12-02 15:41:59 +0545

    bug fixes: setup uncommenting codes and adding installed file in .gitignore

--- app/Console/Commands/SetupCommand.php
+++ app/Console/Commands/SetupCommand.php
@@ -33,29 +33,29 @@ class SetupCommand extends Command
             die();
         }
 
-        // // Application name
-        // $name = $this->ask('What is the name of the Application?', "Makura Creation Technical Assessment By Bishal Shrestha");
-        // $setupService->setupAppName($name);
+        // Application name
+        $name = $this->ask('What is the name of the Application?', "Makura Creation Technical Assessment By Bishal Shrestha");
+        $setupService->setupAppName($name);
 
-        // //Database Setup
-        // $this->info("Database Setup");
-        // $dbConnection = $this->choice("Database Connection?", [
-        //     'mysql',
-        //     'pgsql'
-        // ], 0);
-        // $dbPort = $this->ask('DB PORT:', "3306");
-        // $dbDB = $this->ask('Database Name:', "laravel");
-        // $dbUsername = $this->ask('Database Username:', 'root');
-        // $dbPassword = $this->ask('Database Password:');
-        // $setupService->setupDatabase($dbConnection, $dbPort, $dbDB, $dbUsername, $dbPassword);
+        //Database Setup
+        $this->info("Database Setup");
+        $dbConnection = $this->choice("Database Connection?", [
+            'mysql',
+            'pgsql'
+        ], 0);
+        $dbPort = $this->ask('DB PORT:', "3306");
+        $dbDB = $this->ask('Database Name:', "laravel");
+        $dbUsername = $this->ask('Database Username:', 'root');
+        $dbPassword = $this->ask('Database Password:');
+        $setupService->setupDatabase($dbConnection, $dbPort, $dbDB, $dbUsername, $dbPassword);
 
-        // $this->alert('Migrating in database');
-        // Artisan::call('migrate');
-        // $this->info("Migration Complete");
+        $this->alert('Migrating in database');
+        Artisan::call('migrate');
+        $this->info("Migration Complete");
 
-        // $this->alert("Configuring Passport for JSON Web Token(JWT)");
-        // Artisan::call('passport:token-install');
-        // $this->info("Passport ID and Secret have been configured");
+        $this->alert("Configuring Passport for JSON Web Token(JWT)");
+        Artisan::call('passport:token-install');
+        $this->info("Passport ID and Secret have been configured");
 
         $setupService->addWaterMark();
     }
